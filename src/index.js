515b947e2b23408746a0c14936e5490140a0d54c
Tym-js <42276835+Tym-js@users.noreply.github.com>
2018-12-22 13:28:02 +0900

    onAuthStateChanged to Listen for Login/Register

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./styles.css"
-import "semantic-ui-css/semantic.min.css"
-import App from "./components/App.jsx"
-import Login from "./components/Auth/Login.jsx"
-import Register from "./components/Auth/Register.jsx"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import "semantic-ui-css/semantic.min.css";
+import App from "./components/App.jsx";
+import Login from "./components/Auth/Login.jsx";
+import Register from "./components/Auth/Register.jsx";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom";
+import firebase from "./firebase";
 
-const Root = () => (
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
   <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
-)
-
-ReactDOM.render(<Root />, document.getElementById("root"))
+    <RootWithAuth />
+  </Router>,
+  document.getElementById("root")
+);
