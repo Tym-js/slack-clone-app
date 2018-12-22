cea13c0b46eab039e60326a94b236917ba4e06df
Tym-js <42276835+Tym-js@users.noreply.github.com>
2018-12-22 16:01:19 +0900

    Scaffold App Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,25 @@
-import React from "react"
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import ColorPanel from "./ColorPanel/ColorPanel.jsx";
+import SidePanel from "./SidePanel/SidePanel.jsx";
+import Messages from "./Messages/Messages.jsx";
+import MetaPanel from "./MetaPanel/MetaPanel.jsx";
 
 class App extends React.Component {
   render() {
-    return <div>App</div>
+    return (
+      <Grid columns="equal" className="app">
+        <ColorPanel />
+        <SidePanel />
+        <Grid.Column style={{ marginLeft: 320 }}>
+          <Messages />
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <MetaPanel />
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
-export default App
+export default App;
