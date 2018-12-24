3f441f11edabd2485c33184b1c0967682aa9282e
Tym-js <42276835+Tym-js@users.noreply.github.com>
2018-12-24 17:39:40 +0900

    ProgressBar Component to Represent Image Upload

--- src/components/Messages/MessageForm.jsx
+++ src/components/Messages/MessageForm.jsx
@@ -4,6 +4,7 @@ import firebase from "../../firebase"
 import { Segment, Button, Input } from "semantic-ui-react"
 
 import FileModal from "./FileModal.jsx"
+import ProgressBar from "./ProgressBar.jsx"
 
 class MessageForm extends React.Component {
   state = {
@@ -140,7 +141,14 @@ class MessageForm extends React.Component {
   }
 
   render() {
-    const { errors, message, loading, modal } = this.state
+    const {
+      errors,
+      message,
+      loading,
+      modal,
+      uploadState,
+      percentUploaded
+    } = this.state
 
     return (
       <Segment className="message__form">
@@ -176,6 +184,10 @@ class MessageForm extends React.Component {
             uploadFile={this.uploadFile}
           />
         </Button.Group>
+        <ProgressBar
+          uploadState={uploadState}
+          percentUploaded={percentUploaded}
+        />
       </Segment>
     )
   }
